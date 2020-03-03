0dc3603994a7739d982d6ada3690922fbc8819a3
MARXCBR <824368602@qq.com>
2020-03-03 21:05:08 +0800

    添加USN操作文件 可以实现 USN 文件后缀过滤遍历 FileReferenceNumber 回溯文件目录

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,3 @@
-len
+					// fout << "FileName:" << fileName << endl;
+					// fout << "FileReferenceNumber:" << UsnRecord->FileReferenceNumber << endl;
+					// fout << "ParentFileReferenceNumber:" << UsnRecord->ParentFileReferenceNumber << endl;
